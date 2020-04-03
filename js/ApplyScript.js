79c74068c341965e2c88134e91f3ab1001df2b22
Terry Nippard <11567214+theMidst@users.noreply.github.com>
2020-04-03 17:58:36 -0400

    Updated website progress to April 3rd

--- js/ApplyScript.js
+++ js/ApplyScript.js
@@ -1,3 +1,3 @@
-function nextPageFunction () {
+function buttonToContactInfo () {
     window.location.assign("../apply/contact-info.html")
 }
--- js/homeScript.js
+++ js/homeScript.js
@@ -1,14 +1,10 @@
 // $(document).ready(function() {
   
   /* Toggle between adding and removing the "responsive" class to topnav
-  when the user clicks on the icon */
+  when the user clicks on the icon
+   * no longer needed w/bootstrap navbar */
   function navFunction() {
     var x = document.getElementById("homeTopNav");
-    // if (x.className === "topNav") {
-    //   x.className += " responsive";
-    // } else {
-    //   x.className = "topNav";
-    // }
     if (x.className.includes("responsive")) {
       x.className = x.className.replace(" responsive", "");
     } else {
@@ -19,15 +15,17 @@
   // When the user scrolls down 20px from the top of the document, show the nav bg
   // When the user scrolls to the top of the page, hide the nav bg (to show the banner)
   function scrollFunction() {
+    var x = document.getElementById("topNav");
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      var x = document.getElementById("homeTopNav");
-      if (!x.className.includes("topNavScrolled")) {
-        x.className += " topNavScrolled";
+      if (!x.className.includes("bg-dark")) {
+        x.className += " bg-dark";
+        x.className = x.className.replace("navbar-light", "navbar-dark");
+        document.getElementById("brand").src="images/JP-328×89-REVERSE-Web-Banner.png";
       }
     } else {
-      // remove the topNavScrolled class from the navbar's class list
-      var x = document.getElementById("homeTopNav");
-      x.className = x.className.replace(" topNavScrolled", "");
+      x.className = x.className.replace(" bg-dark", "");
+      x.className = x.className.replace("navbar-dark", "navbar-light");
+      document.getElementById("brand").src="images/JP-328×89-REVERSE-Web-Banner-Black.png";
     }
   }
 
